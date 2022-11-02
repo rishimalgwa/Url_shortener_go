2ddbf131182623855a4b4d020f882672d62c4e64
Rishi Malgwa <rishimalgwa35@gmail.com>
2022-11-02 23:48:45 +0530

    db added

--- api/go.mod
+++ api/go.mod
@@ -4,6 +4,9 @@ go 1.18
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/gofiber/fiber/v2 v2.39.0 // indirect
 	github.com/klauspost/compress v1.15.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,28 @@
 package routes
+
+import (
+	"github.com/go-redis/redis"
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/rishimalgwa/url_shortener_go/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+	r := database.CreateClient(0)
+	defer r.Client()
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short url not found in db",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to db",
+		})
+	}
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
+	return c.Redirect(value, 301)
+}
