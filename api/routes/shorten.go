41386e91db488b2a180b05dffbbe027706387904
Rishi Malgwa <rishimalgwa35@gmail.com>
2022-11-03 22:50:14 +0530

    shorten response

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -87,5 +87,21 @@ func ShortenURL(c *fiber.Ctx) error {
 			"error": "unable to connect to server",
 		})
 	}
+	resp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
 	r2.Decr(database.Ctx, c.IP())
+
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+	resp.CustomShort = os.Getenv("Domain") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
+
 }
